aa56a2647bff4026f46518ce406e17f28102fc12
hopeatina <hopeatina@gmail.com>
2024-08-07 16:05:38 -0500

    Update version number to 0.1.2 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="model-schema-exporter",
-    version="0.1.1",
+    version="0.1.2",
     author="Hope Atina",
     author_email="",
     description="A tool to export model schemas from Django, SQLAlchemy, and FastAPI to JSON",
